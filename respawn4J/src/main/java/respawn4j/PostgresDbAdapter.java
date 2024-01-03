554a4f7bc7c768dd7a6aa44b29d986d527ba250b
Mohammed Kamil <kamlan19922@gmail.com>
2024-01-04 00:52:04 +0400

    added support for skipping tables from reset

--- respawn4J/src/main/java/respawn4j/PostgresDbAdapter.java
+++ respawn4J/src/main/java/respawn4j/PostgresDbAdapter.java
@@ -1,5 +1,9 @@
 package respawn4j;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PostgresDbAdapter {
 
     private final char QUOTE_Character = '"';
@@ -8,29 +12,71 @@ public class PostgresDbAdapter {
 
     public String buildTableCommandText(RespawnerOptions options)
     {
-        String commandText = """
+        StringBuilder commandText = new StringBuilder("""
                 select TABLE_SCHEMA, TABLE_NAME
                         from INFORMATION_SCHEMA.TABLES
                         where TABLE_TYPE = 'BASE TABLE'
-                """;
+                """);
+
+        if (options.tablesToIgnore().length > 0) {
+            var tablesToIgnoreGroups = Arrays.stream(options.tablesToIgnore())
+                    .collect(Collectors.groupingBy(t -> t.schema() != null));
+
+            for (var tableGroup : tablesToIgnoreGroups.entrySet()) {
+                if (tableGroup.getKey()) {
+                    String args = tableGroup.getValue().stream()
+                            .map(table -> "'" + table.schema() + "." + table.name() + "'")
+                            .collect(Collectors.joining(","));
+
+                    commandText.append(" AND TABLE_SCHEMA || '.' || TABLE_NAME NOT IN (").append(args).append(")");
+                } else {
+                    String args = tableGroup.getValue().stream()
+                            .map(table -> "'" + table.name() + "'")
+                            .collect(Collectors.joining(","));
+
+                    commandText.append(" AND TABLE_NAME NOT IN (").append(args).append(")");
+                }
+            }
+        }
 
 
-        commandText += String.format(" AND TABLE_SCHEMA != '{%s}'", InformationSchema);
-        commandText += String.format(" AND TABLE_SCHEMA NOT LIKE '%s%%'", PostgresSchemaPrefix);
+        commandText.append(String.format(" AND TABLE_SCHEMA != '{%s}'", InformationSchema));
+        commandText.append(String.format(" AND TABLE_SCHEMA NOT LIKE '%s%%'", PostgresSchemaPrefix));
 
-        return commandText;
+        return commandText.toString();
     }
 
     public String buildRelationshipCommandText(RespawnerOptions options) {
-        var commandText = """
+        StringBuilder commandText = new StringBuilder("""
                 select tc.table_schema, tc.table_name, ctu.table_schema, ctu.table_name, rc.constraint_name
                  from INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc
                  inner join INFORMATION_SCHEMA.CONSTRAINT_TABLE_USAGE ctu ON rc.constraint_name = ctu.constraint_name
                  inner join INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc ON rc.constraint_name = tc.constraint_name
                  where 1=1
-                """;
+                """);
 
-        return commandText;
+        if (options.tablesToIgnore().length > 0) {
+            var tablesToIgnoreGroups = Arrays.stream(options.tablesToInclude())
+                    .collect(Collectors.groupingBy(t -> t.schema() != null));
+
+            for (var tableGroup : tablesToIgnoreGroups.entrySet()) {
+                if (tableGroup.getKey()) {
+                    String args = tableGroup.getValue().stream()
+                            .map(table -> "'" + table.schema() + "." + table.name() + "'")
+                            .collect(Collectors.joining(","));
+
+                    commandText.append(" AND tc.TABLE_SCHEMA || '.' || tc.TABLE_NAME NOT IN (").append(args).append(")");
+                } else {
+                    String args = tableGroup.getValue().stream()
+                            .map(table -> "'" + table.name() + "'")
+                            .collect(Collectors.joining(","));
+
+                    commandText.append(" AND tc.TABLE_NAME NOT IN (").append(args).append(")");
+                }
+            }
+        }
+
+        return commandText.toString();
     }
 
     public String buildDeleteCommandText(GraphBuilder graph) {
